379e2af68cbe73c9e551ff6edf6ba57ef2e5b368
SinitsaEV <sinitsammi@gmail.com>
2025-04-04 20:59:49 +0300

    Start

---
+++ Assets/Scripts/AlarmSystem.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AlarmSystem : MonoBehaviour
+{
+    [SerializeField] private AlarmSystemElement _door;
+    [SerializeField] private AlarmSystemElement _floor;
+
+    private void Update()
+    {
+        
+    }
+}
